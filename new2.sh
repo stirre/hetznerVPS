c5c15b786393c673f1a7f3dfc1220d22b5062455
stirre <78666388+stirre@users.noreply.github.com>
2021-02-07 11:41:25 +0200

    Create new2.sh

---
+++ new2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+wget -O pihole.sh https://install.pi-hole.net
+bash pihole.sh
+curl -o /etc/lighttpd/lighttpd.conf https://raw.githubusercontent.com/stirre/hetznerVPS/master/lighttpd.conf
+systemctl restart lighttpd
+pihole -up
+
+ufw allow proto tcp from 10.6.0.0/24 to 10.6.0.1 port 53 comment 'wg-pihole-dns-tcp'
+ufw allow proto udp from 10.6.0.0/24 to 10.6.0.1 port 53 comment 'wg-pihole-dns-udp'
+ufw allow proto tcp from 10.6.0.0/24 to 10.6.0.1 port 800 comment 'wg-pihole-admin-http-tcp'
+ufw allow 4711/tcp
+ufw allow 51820/udp
+
+curl -o /etc/pihole/pihole-FTL.conf https://raw.githubusercontent.com/stirre/hetznerVPS/master/pihole-FTL.conf
+sed -i '/^function connectFTL/s/4711/4712/' /var/www/html/admin/scripts/pi-hole/php/FTL.php
+
+systemctl stop systemd-resolved
+systemctl disable systemd-resolved
+ufw enable
+reboot
