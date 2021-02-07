7041693c5f681c3d00505a406d7925710ca35c52
stirre <78666388+stirre@users.noreply.github.com>
2021-02-07 10:55:32 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ echo " # will automatically download the script, but you need to run this manual
 echo " # More information: https://github.com/zzzkeil/Wireguard-DNScrypt-VPN-Server #"
 echo " ##############################################################################"
 echo " ##############################################################################"
-echo " # Version 2021.01.04 - status: update dnscrypt 2.0.45 with blocked_names etc #"
+echo " # Version 1 - status: update dnscrypt 2.0.45 with blocked_names etc #"
 echo " ##############################################################################"
 echo ""
 echo ""
@@ -28,49 +28,12 @@ if [[ "$EUID" -ne 0 ]]; then
 	exit 1
 fi
 #
-### base_setup check
-if [[ -e /root/base_setup.README ]]; then
-     echo "base_setup script installed - OK"
-	 else
-	 wget -O  base_setup.sh https://raw.githubusercontent.com/zzzkeil/base_setups/master/base_setup.sh
-         chmod +x base_setup.sh
-	 echo ""
-	 echo ""
-	 echo " Attention !!! "
-	 echo " My base_setup script not installed,"
-         echo " you have to run ./base_setup.sh manualy now and reboot, after that you can run this script again."
-	 echo ""
-	 echo ""
-	 exit 1
-fi
-#
 ### OS version check
 if [[ -e /etc/debian_version ]]; then
       echo "Debian Distribution"
       else
       echo "This is not a Debian Distribution."
       exit 1
-fi
-#
-### script already installed check
-if [[ -e /root/Wireguard-DNScrypt-VPN-Server.README ]]; then
-     echo
-	 echo
-         echo "Looks like this script is already installed"
-	 echo "This script is only for the first install"
-	 echo ""
-	 echo "To add or remove clients run"
-         echo " ./add_client.sh to add clients"
-         echo " ./remove_client.sh	to remove clients" 
-	 echo ""
-	 echo "More instructions in this file: "
-	 echo "/root/Wireguard-DNScrypt-VPN-Server.README"
-	 echo ""
-	 echo "For - News / Updates / Issues - check my github site"
-	 echo "https://github.com/zzzkeil/Wireguard-DNScrypt-VPN-Server"
-	 echo
-	 echo
-	 exit 1
 fi
 #
 ### create backupfolder for original files
@@ -81,14 +44,6 @@ echo " Make your port settings now:"
 echo "------------------------------------------------------------"
 read -p "Choose your Wireguard Port: " -e -i 51820 wg0port
 echo "------------------------------------------------------------"
-#
-### apt systemupdate and installs	 
-echo
-VERSION_ID=$(cat /etc/os-release | grep "VERSION_ID")
-if [[ "$VERSION_ID" = 'VERSION_ID="10"' ]]; then
-	echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable-wireguard.list
-        printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
-fi
 
 if [[ "$VERSION_ID" = 'VERSION_ID="18.04"' ]]; then
     add-apt-repository ppa:wireguard/wireguard
@@ -98,22 +53,16 @@ if [[ "$VERSION_ID" = 'VERSION_ID="20.04"' ]]; then
     echo " system is ubuntu 20.04 - no ppa:wireguard needed "
 fi
 
-apt update && apt upgrade -y && apt autoremove -y
-apt install qrencode unbound unbound-host python curl linux-headers-$(uname -r) -y 
-apt install wireguard-dkms wireguard-tools -y
+apt update && apt dist-upgrade -y && apt autoremove -y
+
+apt install qrencode python curl
 #
 ### setup ufw and sysctl
 inet=$(ip route show default | awk '/default/ {print $5}')
-ufw allow $wg0port/udp
 cp /etc/default/ufw /root/script_backupfiles/ufw.orig
 cp /etc/ufw/before.rules /root/script_backupfiles/before.rules.orig
 cp /etc/ufw/before6.rules /root/script_backupfiles/before6.rules.orig
 sed -i 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/' /etc/default/ufw
-sed -i "1i# START WIREGUARD RULES\n# NAT table rules\n*nat\n:POSTROUTING ACCEPT [0:0]\n# Allow traffic from WIREGUARD client \n-A POSTROUTING -s 10.8.0.0/24 -o $inet -j MASQUERADE\nCOMMIT\n# END WIREGUARD RULES\n" /etc/ufw/before.rules
-sed -i '/# End required lines/a \\n-A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\n-A INPUT -p udp -m udp --dport 14443 -m conntrack --ctstate NEW -j ACCEPT\n-A INPUT -s 10.8.0.0/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT\n-A INPUT -s 10.8.0.0/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT\n-A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\n-A FORWARD -i wg0 -o wg0 -m conntrack --ctstate NEW -j ACCEPT' /etc/ufw/before.rules
-sed -i '/-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT/a \\n# allow outbound icmp\n-A ufw-before-output -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT\n-A ufw-before-output -p icmp -m state --state ESTABLISHED,RELATED -j ACCEPT\n' /etc/ufw/before.rules
-sed -i "1i# START WIREGUARD RULES\n# NAT table rules\n*nat\n:POSTROUTING ACCEPT [0:0]\n# Allow traffic from WIREGUARD client \n\n-A POSTROUTING -s fd42:42:42:42::/112 -o $inet -j MASQUERADE\nCOMMIT\n# END WIREGUARD RULES\n" /etc/ufw/before6.rules
-sed -i '/# End required lines/a \\n-A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\n-A INPUT -p udp -m udp --dport 14443 -m conntrack --ctstate NEW -j ACCEPT\n-A INPUT -s fd42:42:42:42::1/64 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT\n-A INPUT -s fd42:42:42:42::1/64 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT\n-A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\n-A FORWARD -i wg0 -o wg0 -m conntrack --ctstate NEW -j ACCEPT' /etc/ufw/before6.rules
 cp /etc/sysctl.conf /root/script_backupfiles/sysctl.conf.orig
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 sed -i 's/#net.ipv6.conf.all.forwarding=1/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
@@ -122,157 +71,8 @@ sed -i 's@#net/ipv4/ip_forward=1@net/ipv4/ip_forward=1@g' /etc/ufw/sysctl.conf
 sed -i 's@#net/ipv6/conf/default/forwarding=1@net/ipv6/conf/default/forwarding=1@g' /etc/ufw/sysctl.conf
 sed -i 's@#net/ipv6/conf/all/forwarding=1@net/ipv6/conf/all/forwarding=1@g' /etc/ufw/sysctl.conf
 #
-### setup wireguard keys and configs
-mkdir /etc/wireguard/keys
-chmod 700 /etc/wireguard/keys
 
-touch /etc/wireguard/keys/server0
-chmod 600 /etc/wireguard/keys/server0
-wg genkey > /etc/wireguard/keys/server0
-wg pubkey < /etc/wireguard/keys/server0 > /etc/wireguard/keys/server0.pub
 
-touch /etc/wireguard/keys/client1
-chmod 600 /etc/wireguard/keys/client1
-wg genkey > /etc/wireguard/keys/client1
-wg pubkey < /etc/wireguard/keys/client1 > /etc/wireguard/keys/client1.pub
-
-touch /etc/wireguard/keys/client2
-chmod 600 /etc/wireguard/keys/client2
-wg genkey > /etc/wireguard/keys/client2
-wg pubkey < /etc/wireguard/keys/client2 > /etc/wireguard/keys/client2.pub
-
-touch /etc/wireguard/keys/client3
-chmod 600 /etc/wireguard/keys/client3
-wg genkey > /etc/wireguard/keys/client3
-wg pubkey < /etc/wireguard/keys/client3 > /etc/wireguard/keys/client3.pub
-
-touch /etc/wireguard/keys/client4
-chmod 600 /etc/wireguard/keys/client4
-wg genkey > /etc/wireguard/keys/client4
-wg pubkey < /etc/wireguard/keys/client4 > /etc/wireguard/keys/client4.pub
-
-touch /etc/wireguard/keys/client5
-chmod 600 /etc/wireguard/keys/client5
-wg genkey > /etc/wireguard/keys/client5
-wg pubkey < /etc/wireguard/keys/client5 > /etc/wireguard/keys/client5.pub
-
-### -
-echo "[Interface]
-Address = 10.8.0.1/24
-Address = fd42:42:42:42::1/112
-ListenPort = $wg0port
-PrivateKey = SK01
-# client1
-[Peer]
-PublicKey = PK01
-AllowedIPs = 10.8.0.11/32, fd42:42:42:42::11/128
-# client2
-[Peer]
-PublicKey = PK02
-AllowedIPs = 10.8.0.12/32, fd42:42:42:42::12/128
-# client3
-[Peer]
-PublicKey = PK03
-AllowedIPs = 10.8.0.13/32, fd42:42:42:42::13/128
-# client4
-[Peer]
-PublicKey = PK04
-AllowedIPs = 10.8.0.14/32, fd42:42:42:42::14/128
-# client5
-[Peer]
-PublicKey = PK05
-AllowedIPs = 10.8.0.15/32, fd42:42:42:42::15/128
-# -end of default clients
-" > /etc/wireguard/wg0.conf
-sed -i "s@SK01@$(cat /etc/wireguard/keys/server0)@" /etc/wireguard/wg0.conf
-sed -i "s@PK01@$(cat /etc/wireguard/keys/client1.pub)@" /etc/wireguard/wg0.conf
-sed -i "s@PK02@$(cat /etc/wireguard/keys/client2.pub)@" /etc/wireguard/wg0.conf
-sed -i "s@PK03@$(cat /etc/wireguard/keys/client3.pub)@" /etc/wireguard/wg0.conf
-sed -i "s@PK04@$(cat /etc/wireguard/keys/client4.pub)@" /etc/wireguard/wg0.conf
-sed -i "s@PK05@$(cat /etc/wireguard/keys/client5.pub)@" /etc/wireguard/wg0.conf
-chmod 600 /etc/wireguard/wg0.conf
-
-### -
-echo "[Interface]
-Address = 10.8.0.11/32
-Address = fd42:42:42:42::11/128
-PrivateKey = CK01
-DNS = 10.8.0.1, fd42:42:42:42::1
-[Peer]
-Endpoint = IP01:$wg0port
-PublicKey = SK01
-AllowedIPs = 0.0.0.0/0, ::/0
-" > /etc/wireguard/client1.conf
-sed -i "s@CK01@$(cat /etc/wireguard/keys/client1)@" /etc/wireguard/client1.conf
-sed -i "s@SK01@$(cat /etc/wireguard/keys/server0.pub)@" /etc/wireguard/client1.conf
-sed -i "s@IP01@$(hostname -I | awk '{print $1}')@" /etc/wireguard/client1.conf
-chmod 600 /etc/wireguard/client1.conf
-
-echo "[Interface]
-Address = 10.8.0.12/32
-Address = fd42:42:42:42::12/128
-PrivateKey = CK02
-DNS = 10.8.0.1, fd42:42:42:42::1
-[Peer]
-Endpoint = IP01:$wg0port
-PublicKey = SK01
-AllowedIPs = 0.0.0.0/0, ::/0
-" > /etc/wireguard/client2.conf
-sed -i "s@CK02@$(cat /etc/wireguard/keys/client2)@" /etc/wireguard/client2.conf
-sed -i "s@SK01@$(cat /etc/wireguard/keys/server0.pub)@" /etc/wireguard/client2.conf
-sed -i "s@IP01@$(hostname -I | awk '{print $1}')@" /etc/wireguard/client2.conf
-chmod 600 /etc/wireguard/client2.conf
-
-echo "[Interface]
-Address = 10.8.0.13/32
-Address = fd42:42:42:42::13/128
-PrivateKey = CK03
-DNS = 10.8.0.1, fd42:42:42:42::1
-[Peer]
-Endpoint = IP01:$wg0port
-PublicKey = SK01
-AllowedIPs = 0.0.0.0/0, ::/0
-" > /etc/wireguard/client3.conf
-sed -i "s@CK03@$(cat /etc/wireguard/keys/client3)@" /etc/wireguard/client3.conf
-sed -i "s@SK01@$(cat /etc/wireguard/keys/server0.pub)@" /etc/wireguard/client3.conf
-sed -i "s@IP01@$(hostname -I | awk '{print $1}')@" /etc/wireguard/client3.conf
-chmod 600 /etc/wireguard/client3.conf
-
-echo "[Interface]
-Address = 10.8.0.14/32
-Address = fd42:42:42:42::14/128
-PrivateKey = CK04
-DNS = 10.8.0.1, fd42:42:42:42::1
-[Peer]
-Endpoint = IP01:$wg0port
-PublicKey = SK01
-AllowedIPs = 0.0.0.0/0, ::/0
-" > /etc/wireguard/client4.conf
-sed -i "s@CK04@$(cat /etc/wireguard/keys/client4)@" /etc/wireguard/client4.conf
-sed -i "s@SK01@$(cat /etc/wireguard/keys/server0.pub)@" /etc/wireguard/client4.conf
-sed -i "s@IP01@$(hostname -I | awk '{print $1}')@" /etc/wireguard/client4.conf
-chmod 600 /etc/wireguard/client4.conf
-
-echo "[Interface]
-Address = 10.8.0.15/32
-Address = fd42:42:42:42::15/128
-PrivateKey = CK05
-DNS = 10.8.0.1, fd42:42:42:42::1
-[Peer]
-Endpoint = IP01:$wg0port
-PublicKey = SK01
-AllowedIPs = 0.0.0.0/0, ::/0
-" > /etc/wireguard/client5.conf
-sed -i "s@CK05@$(cat /etc/wireguard/keys/client5)@" /etc/wireguard/client5.conf
-sed -i "s@SK01@$(cat /etc/wireguard/keys/server0.pub)@" /etc/wireguard/client5.conf
-sed -i "s@IP01@$(hostname -I | awk '{print $1}')@" /etc/wireguard/client5.conf
-chmod 600 /etc/wireguard/client5.conf
-#
-### setup unbound
-curl -o /var/lib/unbound/root.hints https://www.internic.net/domain/named.cache
-curl -o /etc/unbound/unbound.conf https://raw.githubusercontent.com/zzzkeil/Wireguard-DNScrypt-VPN-Server/master/configs/unbound.conf
-chown -R unbound:unbound /var/lib/unbound
-#
 ###setup DNSCrypt
 mkdir /etc/dnscrypt-proxy/
 wget -O /etc/dnscrypt-proxy/dnscrypt-proxy.tar.gz https://github.com/jedisct1/dnscrypt-proxy/releases/download/2.0.45/dnscrypt-proxy-linux_x86_64-2.0.45.tar.gz
