a60b4232698503a612d1e97c8522d2854f2732e6
stirre <78666388+stirre@users.noreply.github.com>
2021-02-22 11:04:21 +0200

    Update new.sh

--- new.sh
+++ new.sh
@@ -10,6 +10,11 @@ curl -o /etc/dnscrypt-proxy/dnscrypt-proxy.toml https://raw.githubusercontent.co
 curl -o /etc/systemd/system/dnscrypt-proxy.socket https://raw.githubusercontent.com/stirre/hetznerVPS/master/dnscrypt-proxy.socket
 systemctl restart dnscrypt-proxy.socket
 systemctl restart dnscrypt-proxy
+
+ufw allow 4711/tcp
+ufw allow 51800/udp
+ufw enable
+
 curl -L https://install.pivpn.io > pivpn.sh
 sudo bash pivpn.sh
 
