f447c6eba89971eb7935050d544ec5cdda66f1eb
stirre <78666388+stirre@users.noreply.github.com>
2021-02-23 06:09:05 +0200

    Update new.sh

--- new.sh
+++ new.sh
@@ -2,7 +2,8 @@
 apt update
 apt dist-upgrade -y
 apt autoremove -y
-curl -o /etc/ssh/sshd_config https://raw.githubusercontent.com/stirre/hetznerVPS/master/sshd_config
+sed -i "s/#Port 22/Port 9471/" /etc/ssh/sshd_config
+sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
 service ssh restart
 apt install dnscrypt-proxy -y
 mv /etc/dnscrypt-proxy/dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml.backup
@@ -11,6 +12,7 @@ curl -o /etc/systemd/system/dnscrypt-proxy.socket https://raw.githubusercontent.
 systemctl restart dnscrypt-proxy.socket
 systemctl restart dnscrypt-proxy
 
+ufw allow 9471/tcp
 ufw allow 4711/tcp
 ufw allow 51800/udp
 ufw enable
